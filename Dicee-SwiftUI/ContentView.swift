2065f1e81e00fead820b2ac80b8d5d6a3604f94b
jjcode22 <jjmcoding31@gmail.com>
2024-02-11 06:52:12 +0530

    SwiftUI update

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDice = 1
+    @State var rightDice = 1
+    @State var message = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                Text(message).font(.system(size: 40)).fontWeight(.bold).foregroundStyle(.white
+                )
+                Spacer()
+                HStack {
+                    Dice(diceNum: leftDice)
+                    Dice(diceNum: rightDice)
+                }.padding()
+                Spacer()
+                Button("Roll") {
+                    self.leftDice = Int.random(in: 1...6)
+                    self.rightDice = Int.random(in: 1...6)
+                    if(leftDice > rightDice){
+                        message = "Player 1 wins"
+                    }else if (rightDice > leftDice){
+                        message = "Player 2 wins"
+                    }else{
+                        message = "Draw"
+                    }
+                }.font(.system(size: 40)).fontWeight(.bold).foregroundStyle(.white
+                ).padding(.horizontal).background(.red).clipShape(RoundedRectangle(cornerRadius: 10))
+                Spacer()
+            }
         }
-        .padding()
+    }
+}
+
+struct Dice: View {
+    let diceNum: Int
+    var body: some View {
+        Image("dice\(diceNum)").resizable().aspectRatio(1,contentMode: .fit)
+            .padding()
     }
 }
 
